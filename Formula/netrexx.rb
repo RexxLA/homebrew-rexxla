37a8f9c0da962c7c19e4842af1e7ab3898b67948
René Jansen <rvjansen@xs4all.nl>
2023-04-16 23:29:45 +0200

    upd

--- Formula/netrexx.rb
+++ Formula/netrexx.rb
@@ -5,9 +5,8 @@ class Netrexx < Formula
   sha256 "cfcfe834e7893a01da18e1e5176d7671e8054254a80cf735edf1a8acba3089c5"
   license "ICU"
 
-  depends_on "openjdk" => :build
+  depends_on "openjdk"
   depends_on "make" => :build
-  #conflicts_with "nothing", because: "this reason"
 
   def install
     ENV.deparallelize
@@ -18,6 +17,7 @@ class Netrexx < Formula
     bin.install "bin/nr" => "nr"
     bin.install "bin/nrc.bat" => "nrc.bat"
     bin.install "bin/NetRexxC.bat" => "NetRexxC.bat"
+    bin.install "bin/NetRexxC.cmd" => "NetRexxC.cmd"
     bin.install "bin/pipe" => "pipe"
     bin.install "bin/pipc" => "pipc"
     bin.install "bin/pipe.bat" => "pipe.bat"
